b5c24d487491e3c7946df9e4ae93a2a68d1b7dd1
alvarogo1998 <114082652+alvarogo1998@users.noreply.github.com>
2022-10-13 17:33:33 +0200

    start Ut01Ex06Activity

--- app/src/main/java/com/agalobr/aad_2022/ut01/ex06/presentation/ut01Ex06Activity.kt
+++ app/src/main/java/com/agalobr/aad_2022/ut01/ex06/presentation/ut01Ex06Activity.kt
@@ -1,9 +1,7 @@
 package com.agalobr.aad_2022.ut01.ex06.presentation
 
-import android.content.Context
-import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import com.agalobr.aad_2022.R
 import com.agalobr.aad_2022.ut01.ex06.date.local.UsersSource
 import com.agalobr.aad_2022.ut01.ex06.domain.Address
@@ -11,25 +9,25 @@ import com.agalobr.aad_2022.ut01.ex06.domain.Company
 import com.agalobr.aad_2022.ut01.ex06.domain.Geo
 import com.agalobr.aad_2022.ut01.ex06.domain.Users
 
-class Ut01Ex06Activity : AppCompatActivity() {
+class ut01Ex06Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ut01ex05)
-
+        setContentView(R.layout.activity_ut01ex06)
         init()
     }
     private fun init(){
         val usersSource = UsersSource(
-            getPreferences(Context.MODE_PRIVATE)
+            getPreferences(MODE_PRIVATE)
         )
         val user1 = Users(1, "User1", "Username1", "1111@email.com", Address("street1", "suite1","city1","zipcode1",
-                Geo(1,1)
-            ),"phone1","website1", Company("Company1","Phrase1","bs1"))
+            Geo(1,1)
+        ),"phone1","website1", Company("Company1","Phrase1","bs1")
+        )
         val user2 = Users(2, "User2", "Username2", "2222@email.com", Address("street2", "suite2","city2","zipcode2",
-                Geo(2,2)
-            ),"phone2","website2", Company("Company2","Phrase2","bs2"))
+            Geo(2,2)
+        ),"phone2","website2", Company("Company2","Phrase2","bs2")
+        )
         usersSource.saveUser(user1)
         usersSource.saveUser(user2)
-        
     }
 }
